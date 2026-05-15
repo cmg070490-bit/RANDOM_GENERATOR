8fc64b5643f4268bea62b79996aa9cab17f716d5
cmg070490-bit <cmg070490@gmail.com>
2026-05-15 17:44:19 +0000

    Actualizacion 1

--- src/app.js
+++ src/app.js
@@ -5,7 +5,14 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+const randomice = Math.floor(Math.random() * who.length)
+
+document.getElementById("resultado").textContent = who[Mathh.floow(Math.random() * nombres.lenght)];
+
